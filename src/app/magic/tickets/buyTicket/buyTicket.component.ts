9383064a5e7cc560bea6aae3ef509130654c148d
Lauren Gair <lauren_gair@hotmail.co.uk>
2019-05-04 09:02:45 +0000

    paypal integration

--- src/app/magic/tickets/buyTicket/buyTicket.component.ts
+++ src/app/magic/tickets/buyTicket/buyTicket.component.ts
@@ -1,17 +1,76 @@
 import {
-    Component
+    Component, AfterViewChecked
 } from '@angular/core';
 import {
     BaseMatTableMagicComponent,
     matMagicProviders
 } from "@magic-xpa/angular-material-core";
+import { FormsModule } from '@angular/forms';
+
+declare let paypal: any;
+
+
 @Component({
     selector: 'mga-buyTicket',
     providers: [...matMagicProviders],
     templateUrl: './buyTicket.component.html'
-}) export class buyTicket extends BaseMatTableMagicComponent {
+}) export class buyTicket extends BaseMatTableMagicComponent implements AfterViewChecked{
     displayedColumns = ['clmTicket',
         'clmPrice',
         'Column5',
     ];
+    addScript: boolean = false;
+    paypalLoad: boolean= true;
+    finalAmount: number  = 1;
+    paypalConfig = {
+        env: 'sandbox',
+        client: {
+            sandbox: 'AZFyfSSmLfGTztHZDJtkDHQV5L4HAqwiIKOlSQcPnAeiYXg5ZdfeOruH',
+            production: ''
+        },
+        commit: true,
+        payment: (data, actions) => {
+            return actions.payment.create({
+                payment: {
+                    transactions: [
+                        {amount: {total: this.finalAmount, currency: 'USD'}}
+                    ]
+                }
+            }
+
+            );
+        },
+        onAuthorize: (data, actions) => {
+            actions.payment.execute().then((payment) => {
+                //do something to confirm payment was successful
+            }
+
+            )
+        }
+
+    };
+
+    ngAfterViewChecked(): void {
+        if(!this.addScript){
+            this.addPaypalScript().then(()=>{
+                paypal.button.render(this.paypalConfig, '#paypal-checkout-button');
+                this.paypalLoad =false;
+            }
+
+            )
+        }
+
+    }
+    addPaypalScript() {
+        this.addScript = true;
+        return new Promise((resolve, reject) => {
+            let scripttagElement = document.createElement('script')
+            scripttagElement.src = 'http://www.paypalobjects.com/api/checkout.js';
+            scripttagElement.onload = resolve;
+            document.body.appendChild(scripttagElement)
+        }
+
+        )
+
+    }
 }
